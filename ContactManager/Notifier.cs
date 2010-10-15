281a47b00ec1cd944ebef5ad3727c091f4e8c68c
dhirenv <dhiren@veks.co.uk>
2010-10-15 13:17:12 +0100

    Test Commit

---
+++ ContactManager/Notifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ContactManager
+{
+    [Serializable]
+    public abstract class Notifier : INotifyPropertyChanged
+    {
+        [field: NonSerialized]
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ ContactManager/Presenters/PresenterBase.cs
@@ -0,0 +1,29 @@
+﻿namespace ContactManager.Presenters
+{
+    public class PresenterBase<T> : Notifier
+    {
+        private readonly string _tabHeaderPath;
+        private readonly T _view;
+
+        public PresenterBase(T view)
+        {
+            _view = view;
+        }
+
+        public PresenterBase(T view, string tabHeaderPath)
+        {
+            _view = view;
+            _tabHeaderPath = tabHeaderPath;
+        }
+
+        public T View
+        {
+            get { return _view; }
+        }
+
+        public string TabHeaderPath
+        {
+            get { return _tabHeaderPath; }
+        }
+    }
+}
